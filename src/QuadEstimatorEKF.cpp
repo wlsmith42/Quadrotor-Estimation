9949ee655152e9941a87f0ac2746f0776a53f058
Weston Smith <wlsmith42@students.tntech.edu>
2019-05-10 17:23:25 -0400

    feat: implemented magnetometer update

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -322,8 +322,17 @@ void QuadEstimatorEKF::UpdateFromMag(float magYaw)
   //    (you don't want to update your yaw the long way around the circle)
   //  - The magnetomer measurement covariance is available in member variable R_Mag
   ////////////////////////////// BEGIN STUDENT CODE ///////////////////////////
-
-
+  float d = z(0) - ekfState(6);
+  
+  if(d > F_PI) {
+    z(0) -= 2.f * F_PI;
+  }
+  if(d < -F_PI) {
+    z(0) += 2.f * F_PI;
+  }
+  
+  zFromX(0) = ekfState(6);
+  hPrime(0,6) = 1;
   /////////////////////////////// END STUDENT CODE ////////////////////////////
 
   Update(z, hPrime, R_Mag, zFromX);
